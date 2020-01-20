add159607737472cd0e5daf72bd8da67bfac0a72
TaimurSyed <51291629+TaimurSyed@users.noreply.github.com>
2020-01-19 22:15:35 -0500

    Create BigIntFormatException.java

---
+++ BigIntFormatException.java
@@ -0,0 +1,13 @@
+
+public class BigIntFormatException extends RuntimeException {
+
+	public BigIntFormatException() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+	
+	public BigIntFormatException(String message) {
+		super(message);
+	}
+
+}
